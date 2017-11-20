2012d822b545a5f32987dd08a96da77e3c9fd318
mirelatv <noelia.mirela@gmail.com>
2017-11-20 03:06:49 -0500

    subiendo trabajo de Trello

--- js/app.js
+++ js/app.js
@@ -6,23 +6,23 @@ window.addEventListener('load', function() {
   var text = document.createElement('input');
   var send = document.createElement('button');
   var store = document.createTextNode('guardar');
-  /*var transparentButton = document.createElement('button');*/
-  /*var textAnchor = document.createTextNode('Anadir una tarjeta');*/
 
   var box = document.createElement('div');
   var txt = document.createElement('input');
   var btn = document.createElement('button');
   var sv = document.createTextNode('guardar');
 
-  var containerParagraph = document.createElement('ul');
-  var newParagraph = document.createElement('li');
-  var newInfo = document.createElement('span');
-  newInfo.setAttribute('class', 'textSpan');
 
-  /*var Textarea = document.createElement('textarea');*/
-  var input3 = document.createElement('input');
-  /*var link = document.createElement('a');*/
+  var newParagraph = document.createElement('p');
+  newParagraph.className = ('parrf');
 
+  var anchor = document.createElement('a');
+  anchor.setAttribute('href', '#textarea');
+  anchor.className = 'link';
+  anchor.innerHTML = 'anadiendo texto';
+
+  var form = document.createElement('form');
+  var texta = document.createElement('textarea');
 
   /* Primer Evento*/
   power.addEventListener('click', function(evento) {
@@ -30,6 +30,7 @@ window.addEventListener('load', function() {
     father.appendChild(text);
     father.appendChild(send);
     container.appendChild(father);
+
     /* adhiriendo clases*/
     father.classList.add('div');
     text.classList.add('input');
@@ -41,37 +42,56 @@ window.addEventListener('load', function() {
 
   /* segundo Evento*/
   send.addEventListener('click', function(evento) {
-    console.log(evento);
     btn.appendChild(sv);
     box.appendChild(txt);
     box.appendChild(btn);
     container.appendChild(box);
-    /*anchor.appendChild(textAnchor);*/
     /* asignando clases*/
     box.classList.add('bx');
     txt.classList.add('input');
     btn.classList.add('button');
-    /*anchor.classList.add('a');*/
-    /*boton.claasList.add('a');*/
-    newParagraph.classList.add('childText');
-    /*anchor.setAttribute('href', '#textarea');*/
-    father.appendChild(containerParagraph);
-    containerParagraph.appendChild(newParagraph);
-    newParagraph.appendChild(newInfo);
+
     /* funcion*/
     function insertAfter(container, box) {
       if (container.nextSibling) {
         container.parentNode.insertBefore(box, container.nextSibling);
       } else {
-        container.parentNode.appendChild(i);
+        container.parentNode.appendChild(box);
       }
     }
-    /*father.replaceChild(anchor, send);*/
-    send.classList.add('link');
-    send.innerHTML = 'anadiendo texto';
-    newInfo.textContent = text.value;
-    father.removeChild(text);
-    father.insertBefore(newParagraph, father.firstElementChild);
 
+    father.replaceChild(anchor, send);
+    send.innerHTML = 'anadiendo texto';
+    father.removeChild(text);
+    newParagraph.textContent = text.value;
+    father.insertBefore(newParagraph, father.firstElementChild);
+  });
+
+  /* Tercer Evento*/
+  anchor.addEventListener('click', function() {
+    form.appendChild(texta);
+
+    father.appendChild(form);
+    father.insertBefore(form, anchor);
+
+    /* Inserto antes  del   segundo hijo*/
+    var ul = document.createElement('ul')
+    var lis = document.createElement('li');
+    var sp = document.createElement('span');
+    lis.appendChild(sp);
+    ul.appendChild(lis);
+    father.appendChild(ul);
+
+    sp.classList.add('sp');
+    lis.classList.add('lis');
+    sp.textContent = texta.value;
+    ul.insertBefore(lis, ul.firstElementChild)
+    father.insertBefore(ul, father.firstElementChild.nextSibling);
+    texta.value = ('');
+
+    texta.setAttribute('cols', '40');
+    texta.setAttribute('rows', '8')
+    father.style.height = 'auto';
+    father.style.height = father.scrollHeight + 'px';
   });
 });
